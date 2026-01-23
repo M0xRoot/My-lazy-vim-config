2256b725f4ac126853c5f28626bad7b9f7677c45
Xcoder203 <mrg19178@gmail.com>
2026-01-23 22:39:22 +0100

    sonokai theme config

--- lua/plugins/colorsheme.lua
+++ lua/plugins/colorsheme.lua
@@ -1,13 +1,47 @@
 return {
     {
         "sainnhe/sonokai",
-        -- priority = 1000,
-        lazy = true,
+        priority = 1000,
+        lazy = false,
         config = function()
             vim.g.sonokai_transparent_background = "1"
             vim.g.sonokai_enable_italic = "1"
-            vim.g.sonokai_style = "atlantis"
+            vim.g.sonokai_style = "andromeda"
             vim.cmd.colorscheme("sonokai")
+            vim.cmd([[
+                " Normal text background
+                hi Normal guibg=NONE ctermbg=NONE
+                hi NormalNC guibg=NONE ctermbg=NONE
+
+                " Floating windows
+                hi NormalFloat guibg=NONE ctermbg=NONE
+                hi FloatBorder guibg=NONE ctermbg=NONE
+
+                " Command-line popups / completion menu
+                hi Pmenu guibg=NONE ctermbg=NONE
+                hi PmenuSel guibg=NONE ctermbg=NONE
+                hi PmenuThumb guibg=NONE ctermbg=NONE
+
+                " Telescope windows (if installed)
+                hi TelescopeNormal guibg=NONE ctermbg=NONE
+                hi TelescopeBorder guibg=NONE ctermbg=NONE
+                hi TelescopePrompt guibg=NONE ctermbg=NONE
+                hi TelescopePromptBorder guibg=NONE ctermbg=NONE
+
+                " Lazy.nvim windows
+                hi FloatTitle guibg=NONE ctermbg=NONE
+                hi LazyNormal guibg=NONE ctermbg=NONE
+                hi LazyButton guibg=NONE ctermbg=NONE
+
+                " Cursorline always off
+                hi CursorLine guibg=NONE ctermbg=NONE
+                hi CursorLineNr guifg=#ff9e64
+                hi MatchParen guibg=NONE ctermbg=NONE gui=bold
+
+                " Line numbers
+                hi LineNr guifg=#5a5a5a
+                hi CursorLineNr guifg=#ff9e64
+            ]])
         end,
     },
     {
@@ -24,8 +58,8 @@ return {
     },
     {
         "folke/tokyonight.nvim",
-        priority = 1000,
-        lazy = false,
+        -- priority = 1000,
+        lazy = true,
         config = function()
             require("tokyonight").setup({
                 style = "storm", -- closest to Sonokai Atlantis (dark + contrast)
